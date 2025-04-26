17a12f6c13cef1a8c0795b6706f57df47f60dbc1
12210010.gcit <12210010.gcit@rub.edu.bt>
2025-04-27 01:27:58 +0600

    get by other id

--- maintenancemicroservice/src/main/java/bt/nhdcl/maintenancemicroservice/repository/PreventiveMaintenanceReportRepository.java
+++ maintenancemicroservice/src/main/java/bt/nhdcl/maintenancemicroservice/repository/PreventiveMaintenanceReportRepository.java
@@ -1,10 +1,15 @@
 package bt.nhdcl.maintenancemicroservice.repository;
 
 import bt.nhdcl.maintenancemicroservice.entity.PreventiveMaintenanceReport;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PreventiveMaintenanceReportRepository extends MongoRepository<PreventiveMaintenanceReport, String> {
     // You can add custom query methods here if needed
+    List<PreventiveMaintenanceReport> findByPreventiveMaintenanceID(String preventiveMaintenanceID);
+
 }
